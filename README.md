204f2a057136c695df5b1a9f4db1083415c23e61
Ilya Tampio <quakumei@gmail.com>
2022-04-14 15:13:59 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# zharko-formatter
-Форматтер кода для упрощения жизни 3530904/10002
+<h1 align="center">
+  <br>
+  <a href="https://antizharko.herokuapp.com"><img src="static/android-chrome-512x512.png" alt="Jarkotik" width="200"></a>
+  <br>
+  <br>
+  <a href="https://antizharko.herokuapp.com">AntiZharko</a>
+  <br>
+</h1>
+
+<h4 align="center">Веб-интерфейс и конфиг для clang-format 14 для форматирования лабораторных работ Жарко в соответствии с Coding Guidelines</h4>
+
+<p align="center">
+  <a href="https://antizharko.herokuapp.com">Сайт</a> •
+  <a href="#key-features">Фичи</a> •
+  <a href="#download">.clang-format</a> •
+  <a href="#license">Лицензия</a> •
+  <a href="#credits">Стек</a> •
+  <a href="#contact">Автор</a>
+</p>
+
+![screenshot](docs/demonstration.gif)
+
+## Фичи
+
+- Форматирование
+  - Да, оно делает ту самую лесенку в списках инициализации из CG
+  - Да, оно убирает trailing whitespaces
+  - Да, оно добавляет пустую строку в конце вашего кода
+  - Да, оно фиксит line endings (CRLF)
+  - Если найдете проблемы в форматировании, сообщите [мне](https://vk.com/druzhelubnyy)
+- [Веб-интерфейс](https://antizharko.herokuapp.com) с подсветкой синтаксиса с помощью [Codemirror](https://codemirror.net/)
+- При желании, можно запустить свой сервер используя только Python3 - clang-format 14 доступен через pip!
+- Если вам не нравится использовать веб-интерфейс, можете настроить форматтер внутри вашего редактора с помощью исходного конфига .clang-format (в том числе, и отредактировать его на свой лад!)
+
+## Скачать
+
+Скачать .clang-format можно прямо из репозитория, или по [ссылке](.clang-format).
+
+## Стек
+
+Этот проект написан с помощью:
+
+- [clang-format](https://clang.llvm.org/docs/ClangFormat.html)
+- [Flask](https://flask.palletsprojects.com/en/2.1.x/)
+- [CodeMirror](http://codemirror.net/)
+- [Catppuccin](https://github.com/catppuccin/catppuccin)
+- [gunicorn](https://gunicorn.org/)
+- [Heroku](https://www.heroku.com)
+
+## Лицензия
+
+MIT
+
+---
+
+## Автор
+
+> Автор &nbsp;&middot;&nbsp; [vk.com/druzhelubnyy](https://vk.com/id388032588) &nbsp;&middot;&nbsp;
+> GitHub [@Quakumei](https://github.com/Quakumei) &nbsp;&middot;&nbsp;
+> Telegram [@Quakumei](https://telegram.me/Quakumei)
---
+++ ZFServer.py
@@ -0,0 +1,128 @@
+from flask import Flask, render_template, request, redirect
+import os
+import sys
+import hashlib
+import pickle
+import asyncio
+
+# Check for clang-format
+
+
+def system_check_clang(clang: str = 'clang-format') -> int:
+    # returns clang-format version and -1 if no clang-format
+    cmd = f"{clang} --version > temp/log.txt"
+    retcode = os.system(cmd)
+    if retcode != 0:
+        print(f"[WARN] : {clang} not found.")
+        return -1
+    clang_string = ""
+    with open("temp/log.txt", "r") as f:
+        clang_string = f.read()[:-1]
+    print("[LOG] : Detected clang-format - ", clang_string)
+    return int(clang_string.split(" ")[2].split(".")[0])
+
+
+def install_pip_clang() -> bool:
+    # installs clang-format 14 via pip (04.14.2022)
+    print(f"[LOG] : Trying to install clang-format 14 via pip...")
+    cmd = f"pip install clang-format"
+    retcode = os.system(cmd)
+    return retcode == 0
+
+
+async def update_state(new_config: dict,  variables_pickle_file: str):
+    with open(variables_pickle_file, "wb") as f:
+        pickle.dump(new_config, f)
+
+
+def load_state(variables_pickle_file: str) -> dict:
+    try:
+        with open(variables_pickle_file, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError as e:
+        print("[WARN] : Config file not found, using default")
+        default_config = {
+            "button_pressed_times": 0,
+        }
+        update_state(default_config, variables_pickle_file)
+        return default_config
+    except Exception as e:
+        print(
+            "[ERR] : Something went exceptionally wrong with config file, exitting...: ", e)
+        sys.exit(2)
+
+
+STATE_PICKLE = "config.pickle"
+state_dict = load_state(STATE_PICKLE)
+
+CLANG_FORMAT_EXEC = "clang-format"
+CLANG_FORCE_CHECK_14 = True
+clang_version = system_check_clang(CLANG_FORMAT_EXEC)
+
+
+def force_clang_updated(CLANG_FORMAT_EXEC: str):
+    change = False
+    if (clang_version < 14):
+        print("[LOG] : Installing pip clang-format... (required version is 14+)")
+        success = install_pip_clang()
+        change = True
+        if (success):
+            print("[LOG] : pip clang-format installed successfully")
+            CLANG_FORMAT_EXEC = 'clang-format'  # clang-format 14+
+    if change and not system_check_clang(CLANG_FORMAT_EXEC):
+        print("[ERR] : clang-format not found and/or automatic installation failed. Exitting... (use clang-format-14 for the best experience)")
+        sys.exit(2)
+
+
+if clang_version == -1 or CLANG_FORCE_CHECK_14:
+    force_clang_updated(CLANG_FORMAT_EXEC)
+loop = asyncio.get_event_loop()  # Kosteel
+
+print(
+    f"[LOG] : ANTIZHARKO has already been used {state_dict['button_pressed_times']} times")
+app = Flask(__name__)
+
+
+def formatted(code: str) -> str:
+    buffer_name = hashlib.md5(code.encode()).hexdigest() + "_buffer"
+
+    # Newline in the end of file check (unfortunately not found in clang-format 14)
+    if code[-1] != '\n':
+        code += '\n'
+
+    with open(f"temp/{buffer_name}.cpp", "w") as f:
+        f.write(code)
+
+    cmd = f"{CLANG_FORMAT_EXEC} temp/{buffer_name}.cpp > temp/{buffer_name}_formatted.cpp"
+    retcode = os.system(cmd)
+    if retcode != 0:
+        return "ERROR"
+
+    cmd = f"rm temp/{buffer_name}.cpp temp/{buffer_name}_formatted.cpp"
+    formatted_code = ""
+    with open(f"temp/{buffer_name}_formatted.cpp", "r") as f:
+        formatted_code = f.read()
+        retcode = os.system(cmd)
+    return formatted_code
+
+
+@ app.route("/", methods=["GET"])
+def only_route():
+    return render_template("index.html",
+                           button_pressed_times=state_dict["button_pressed_times"],
+                           jarkotik="static/jarkotik/jarkotik.jpg"), 200
+
+
+@ app.route('/', methods=["POST"])
+def button():
+    code = request.form.get('input_ta')
+    if code == "":
+        return redirect('/')
+    # le Kosteel for saving state at all times
+    state_dict["button_pressed_times"] += 1
+    loop.run_until_complete(update_state(state_dict, STATE_PICKLE))
+    return render_template("index.html",
+                           formatted_code=formatted(code),
+                           code=code,
+                           button_pressed_times=state_dict["button_pressed_times"],
+                           jarkotik="static/jarkotik/jarkotik_ears.gif"), 200
